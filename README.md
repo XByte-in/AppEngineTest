41bac5eac3cc914ee341d67225888a2ec2d25586
Pranshu Gupta <pranshu.gupta@bluestacks.com>
2024-01-21 00:53:21 +0530

    Updated

--- README.md
+++
@@ -1,2 +0,0 @@
-# AppEngineTest
-Testing AppEngine
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import webapp2
+import flask
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers["Content-Type"] = "text/plain"
-        self.response.write("Hello, World!")
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = flask.Flask(__name__)
 
-app = webapp2.WSGIApplication(
-    [
-        ("/", MainPage),
-    ],
-    debug=True,
-)
+
+@app.get("/")
+def hello():
+    """Return a friendly HTTP greeting."""
+    return "Hello World!\n"
+
+
+if __name__ == "__main__":
+    # Used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host="localhost", port=8080, debug=True)
